99f18a3440baf149b54ea32811972a51cf79fdea
Jack Lawson <ajacksified@gmail.com>
2020-10-01 09:03:50 -0400

    Show name

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,14 @@
-import { useSession } from "@inrupt/solid-ui-react";
+import { useSession, Text, CombinedDataProvider } from "@inrupt/solid-ui-react";
 
 export default function Profile() {
   const { session } = useSession();
   const { webId } = session.info;
 
   return (
-    <h1>{webId}</h1>
+    <CombinedDataProvider datasetUrl={webId} thingUrl={webId}>
+      <h1>
+        <Text property="http://www.w3.org/2006/vcard/ns#fn" />
+      </h1>
+    </CombinedDataProvider>
   );
 }
