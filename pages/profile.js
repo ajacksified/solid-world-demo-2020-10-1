1fbbc7936862ee40f871fad584c8bbaf9d8842f7
Jack Lawson <ajacksified@gmail.com>
2020-10-01 09:05:14 -0400

    Edit toggle

--- pages/profile.js
+++ pages/profile.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import { useSession, Text, CombinedDataProvider } from "@inrupt/solid-ui-react";
 
 export default function Profile() {
   const { session } = useSession();
   const { webId } = session.info;
+  const [editing, setEditing] = useState(false);
 
   return (
     <CombinedDataProvider datasetUrl={webId} thingUrl={webId}>
       <h1>
-        <Text property="http://www.w3.org/2006/vcard/ns#fn" />
+        <Text
+          property="http://www.w3.org/2006/vcard/ns#fn"
+          edit={editing}
+          autosave
+        />
       </h1>
+
+      <button onClick={() => setEditing(!editing)}>Edit</button>
     </CombinedDataProvider>
   );
 }
